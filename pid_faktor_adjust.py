64bf6e34f54ca489aba23a56d8710ddfc0cb42b5
andi <andi.muehlmann@arcor.de>
2022-04-11 11:49:51 +0200

    changes in training of agent

--- pid_faktor_adjust.py
+++ pid_faktor_adjust.py
@@ -17,33 +17,31 @@ import pygame
 from pid_controller import PID_Controller
 from graph_repr import Graph_Repr
 
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 def random_num_p_or_n(upper_limit):
     if random.randint(0, 1) == 1:
         return random.random() * upper_limit
     return -random.random() * upper_limit
 
 class Fly_Env(gym.Env):
-    def get_state(self):
-        state = np.array([*self.last_errors, self.pid_controller.differentiator, self.pid_controller.integrator,\
-            self.pid_controller.p_faktor, self.pid_controller.i_faktor, self.pid_controller.d_faktor])
-        return state
-
     def __init__(self, maximum):
-        self.time_available = 15 #in s
+        self.time_available = 30 #in s
         self.delta_time = 0.01
         self.clock = pygame.time.Clock()
         
-        self.time_without_target_change = 0.4
-        self.time_without_faktor_change = 0.4
+        self.time_without_target_change = 0.2
+        self.time_without_faktor_change = 0.5
 
         self.last_target_change = self.time_available
         self.last_faktor_change = self.time_available
 
-        self.target_change = 0.4
+        self.target_change = 0.2
         self.faktor_change = 0.05
         self.faktor = random.random()
 
-        self.checks_per_second = 100
+        self.checks_per_second = 500
 
         self.inaccuracy = 0.05
 
@@ -59,8 +57,11 @@ class Fly_Env(gym.Env):
 
         self.acc = 0
         self.last_acc = 0
+        
+        self.rpm = 0
 
-        self.target = random_num_p_or_n(self.target_change) * 2
+        random_number = random_num_p_or_n(self.target_change)
+        self.target = (random_number / abs(random_number)) * self.target_change * 5
         self.on_target_start = None
 
         self.pid_controller = PID_Controller(self.target - self.pos, self.pos, self.maximum)
@@ -68,65 +69,32 @@ class Fly_Env(gym.Env):
         self.pid_controller.i_faktor = 0
         self.pid_controller.d_faktor = 0
 
-        amount_last_errors = 50 
+        amount_last_errors = 5
         self.last_errors = []
         for _ in range(amount_last_errors):
             self.last_errors.append(0)
 
         self.action_space = gym.spaces.Discrete(7)
-        self.observation_space = gym.spaces.Box(float('-inf'), float('inf'), shape=(amount_last_errors + 5,))
+        self.observation_space = gym.spaces.Box(float('-inf'), float('inf'), shape=(amount_last_errors + 7,))
 
+        self.was_reset = False
+
+    def get_state(self):
+        state = np.array([*self.last_errors, self.acc, self.vel, self.pid_controller.differentiator, self.pid_controller.integrator,\
+            self.pid_controller.p_faktor, self.pid_controller.i_faktor, self.pid_controller.d_faktor])
+        return state
 
     def step(self, action):
-        self.time_available -= self.delta_time
-
-        if action == 1:
-            self.pid_controller.p_faktor += 1
-
-        elif action == 2:
-            self.pid_controller.p_faktor -= 1
-
-        elif action == 3:
-            self.pid_controller.i_faktor += 1
-
-        elif action == 4:
-            self.pid_controller.i_faktor -= 1
-
-        elif action == 5:
-            self.pid_controller.d_faktor += 1
-
-        elif action == 6:
-            self.pid_controller.d_faktor -= 1
+        self.perform_action(action)
 
         measured_pos = self.pos + random_num_p_or_n(self.inaccuracy) 
-        rpm = self.pid_controller.give_rpm(self.target - measured_pos, measured_pos) 
-        if abs(rpm) > 1000:
-            self.reset()
-
-        self.acc = self.faktor * rpm
-        if abs(self.acc) > 100:
-            self.reset()
-
+        self.rpm = self.pid_controller.give_rpm(self.target - measured_pos, measured_pos) 
+        self.acc = self.faktor * self.rpm
         self.vel += (self.acc + self.last_acc) / 2 * self.delta_time
-        if abs(self.vel) > 100:
-            self.reset()
-
         self.pos += (self.vel + self.last_vel) / 2 * self.delta_time
-        if abs(self.pos) > 1000:
-            self.reset()
 
 
-        reward = ((-abs(self.target - self.pos) + 0.03) * 10) ** 3
-        if reward > 0:
-            if self.on_target_start is None:
-                self.on_target_start = self.time_available
-
-            reward ** (1 + self.on_target_start - self.time_available)
-        else:
-            self.on_target_start = None
-
-        reward -= (self.acc * 0.01) ** 2
-
+        self.time_available -= self.delta_time
         if self.time_available <= 0:
             done = True
         else:
@@ -147,20 +115,54 @@ class Fly_Env(gym.Env):
         self.last_errors.append(self.pid_controller.iir_error.outputs[-1])
         self.last_errors.pop()
 
-        return self.get_state(), reward, done, info
+        return self.get_state(), self.get_reward() / 10**20, done, info
+
+    def perform_action(self, action):
+        if action == 1:
+            self.pid_controller.p_faktor += 0.1
+
+        elif action == 2:
+            self.pid_controller.p_faktor -= 0.1
+
+        elif action == 3:
+            self.pid_controller.i_faktor += 0.1
+
+        elif action == 4:
+            self.pid_controller.i_faktor -= 0.1
+
+        elif action == 5:
+            self.pid_controller.d_faktor += 0.1
+
+        elif action == 6:
+            self.pid_controller.d_faktor -= 0.1
+
+    def get_reward(self):
+        if abs(self.rpm) > 10000 or abs(self.acc) > 500 or abs(self.vel) > 500 or abs(self.pos) > 500:
+            self.was_reset = True
+            self.reset()
+
+        reward = (-abs(self.target - self.pos) + 0.03) * 100
+        if reward > 0:
+            reward = ((reward * 2) + 1) ** 8
+        else:
+            reward -= abs((reward - 1) ** 3)
+        reward -= (self.acc * 1/2) ** 4
+
+        if self.was_reset:
+            reward -= 10**20
+            self.was_reset = False
+
+        return reward
 
     def render(self, mode='human'):
         self.clock.tick(self.checks_per_second)
         if self.graph == 0:
-            self.graph = Graph_Repr(1600, 1000)
+            self.graph = Graph_Repr(2200, 1200)
 
         self.graph.add_point(self.pos)
 
         self.graph.target = self.target 
         self.graph.re_draw()
-
-        print(f'p: {self.pid_controller.p_faktor}, i: {self.pid_controller.i_faktor}, d: {self.pid_controller.d_faktor}')
-
 
     def reset(self):
         self.pos = 0
@@ -172,14 +174,19 @@ class Fly_Env(gym.Env):
         self.acc = 0
         self.last_acc = 0
 
-        random_number = random_num_p_or_n(self.faktor_change) * 2
+        self.rpm = 0
+
+        random_number = random_num_p_or_n(self.faktor_change)
         if self.faktor + random_number <= 0:
             self.faktor -= random_number
+        else:
+            self.faktor += random_number
 
-        self.target = random_num_p_or_n(self.target_change)
+        random_number = random_num_p_or_n(self.target_change)
+        self.target = (random_number / abs(random_number)) * self.target_change * 5
         self.on_target_start = None
 
-        self.time_available = 15 # in s
+        self.time_available = 30 # in s
 
         self.pid_controller = PID_Controller(self.target - self.pos, self.pos, self.maximum)
 
@@ -199,16 +206,18 @@ class Fly_Env(gym.Env):
 def build_model(states, actions):
     model = Sequential()
     model.add(Flatten(input_shape=(1,states)))
-    model.add(Dense(24, activation = 'relu'))
-    model.add(Dense(24, activation = 'relu'))
+    model.add(Dense(100, activation = 'relu'))
+    model.add(Dense(50, activation = 'relu'))
+    model.add(Dense(25, activation = 'relu'))
+    model.add(Dense(10, activation = 'relu'))
     model.add(Dense(actions, activation = 'linear'))
     return model
 
 def build_agent(model, actions):
     policy = BoltzmannQPolicy()
-    memory = SequentialMemory(limit = 50000, window_length = 1)
-    dqn = DQNAgent(model = model, memory = memory, policy = policy,
-        nb_actions = actions, nb_steps_warmup = 10, target_model_update = 1e-2)
+    memory = SequentialMemory(limit=50000, window_length=1)
+    dqn = DQNAgent(model=model, memory=memory, policy=policy,
+        nb_actions=actions, nb_steps_warmup=100, target_model_update=1e-2)
     return dqn
 
 env = Fly_Env(6000)
@@ -219,8 +228,8 @@ model = build_model(states, actions)
 model.summary()
 
 dqn = build_agent(model, actions)
-dqn.compile(Adam(lr = 1e-3), metrics=['mae'])
-dqn.fit(env, nb_steps = 1000000, visualize = False, verbose = 1)
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+dqn.fit(env, nb_steps=10000000, visualize=True, verbose=1)
 input('ready?')
 
 _ = dqn.test(env, nb_episodes=15, visualize=True)
