db6ec8bae909ba70f94e16b89f7a9751ab0942f9
Andreas Muehlmann <andi.muehlmann@arcor.de>
2022-05-26 20:58:06 +0200

    abstrakt algorithm so robust and adaptive control possible

--- flight_control.py
+++ flight_control.py
@@ -1,4 +1,17 @@
+import sys
+import os
+import pygame
+from copy import deepcopy
 from abc import ABCMeta, abstractmethod
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+child = os.path.join(current_dir, 'interfaces')
+sys.path.append(child)
+from sim_interface import SimInterface
+from bluetooth_interface import BluetoothInterface
+
+from transform_input import give_heights
+
 
 '''
 (fl) rotor2 -> \ / <- rotor1 (fr)
@@ -8,7 +21,50 @@ from abc import ABCMeta, abstractmethod
 [rotor1, rotor2, rotor2, rotor4]
 '''
 
+
 class FlightControl(metaclass=ABCMeta):
+    def __init__(self):
+        self.rotor_controllers = [self.create_rotor_controller() for _ in range(4)]
+
+        self.interface_control = SimInterface()
+        self.interface_user = BluetoothInterface()
+
+        self.frequency = 100
+        self.clock = pygame.time.Clock()
+
     @abstractmethod
-    def give_outputs(self, measurements):
+    def create_rotor_controller():
+        pass
+
+    def run(self):
+        prev_measurements = None
+        while True:
+            self.clock.tick(self.frequency)
+            
+            measurements = self.interface_control.give_measurements()
+
+            if len(measurements) == 4:
+                prev_measurements = deepcopy(measurements)
+            else:
+                print('failure in collecting measurements or in measuring')
+                if prev_measurements is None:
+                    continue
+                measurements = deepcopy(prev_measurements)
+
+            inputs = self.interface_user.give_inputs()
+            rotor_targets = give_heights(inputs[0], inputs[1])
+
+            outputs = self.give_outputs(rotor_targets, measurements)
+            self.interface_control.send_outputs(outputs)
+
+    def give_outputs(self, targets, measurements):
+        outputs = []
+        for i, measurement in enumerate(measurements):
+            output = self.give_output_rotor_controller(self.rotor_controllers[i], targets[i], measurement)
+            outputs.append(output)
+
+        return outputs
+
+    @abstractmethod
+    def give_output_rotor_controller(self, controller, target, measurement):
         pass
--- robust_flight_control.py
+++ robust_flight_control.py
@@ -1,58 +1,17 @@
 import sys
 import os
-import pygame
-from copy import deepcopy
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
 child = os.path.join(current_dir, 'pid_controller')
 sys.path.append(child)
 from pid_controller import PidController
 
-child = os.path.join(current_dir, 'interfaces')
-sys.path.append(child)
-from sim_interface import SimInterface
-from bluetooth_interface import BluetoothInterface
-
 from flight_control import FlightControl
-from transform_input import give_heights
 
 
 class RobustFlightControl(FlightControl):
-    def __init__(self):
-        self.frequency = 100
-        self.rotor_controllers = [PidController(45, 20, 30, 0.8, 3, 1000) for i in range(4)]
+    def create_rotor_controller(self):
+        return PidController(45, 20, 30, 0.9, 3, 1000)
 
-        self.interface_control = SimInterface()
-        self.interface_user = BluetoothInterface()
-
-        self.clock = pygame.time.Clock()
-
-    def run(self):
-        prev_measurements = None
-        while True:
-            self.clock.tick(self.frequency)
-            
-            measurements = self.interface_control.give_measurements()
-
-            if len(measurements) == 4:
-                prev_measurements = deepcopy(measurements)
-            else:
-                print('failure in collecting measurements or in measuring')
-                if prev_measurements is None:
-                    continue
-                measurements = deepcopy(prev_measurements)
-
-            outputs = self.give_outputs(measurements)
-            self.interface_control.send_outputs(outputs)
-
-    def give_outputs(self, measurements):
-        inputs = self.interface_user.give_inputs()
-        rotor_targets = give_heights(inputs[0], inputs[1])
-
-        outputs = []
-        for i, measurement in enumerate(measurements):
-            outputs.append(self.rotor_controllers[i].give_output(rotor_targets[i] - measurement, measurement))
-
-        return outputs
-
+    def give_output_rotor_controller(self, controller, target, measurement):
+        return controller.give_output(target - measurement, measurement)
