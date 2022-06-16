18a4c12437aa21bf4e105e29fba1dfd65e2b7b2e
Andreas Muehlmann <andi.muehlmann@arcor.de>
2022-06-16 17:44:15 +0200

    config grouped and not a mess anymore

--- config.py
+++ config.py
@@ -1,49 +1,49 @@
 import os
 
-# The environments should be configured in quadflightcontrol/envs
-from pos_env import PosEnv
-from vel_env import VelEnv
+
+# ENVIRONMENT
+env = 'vel_env' # The environments should be configured in quadflightcontrol/envs
+total_time = 30 # per episode in s
+delay = 0.05 # in s
+delta_time = 0.05 # in s
 
 
-env = 'vel_env'
-
+# SIMULATION IN ENVS
+episodes = 1000
+range_avg = 10
+fps = 5 / delta_time # 1 / delta_time is normal speed
 window_width = 1200
 window_height = 800
 
-total_time = 30 #in s
-delay = 0.05
-delta_time = 0.05
-fps = 5 / delta_time # 1 / delta_time is normal speed
 
+# CONTROLLER
+controller = 'adaptive_pid_controller'
 max_output_controller = 1000
-
-amount_prev_observations = 20
 
 
 # PID_CONTROLLER
 p_faktor = 400
 i_faktor = 150
 d_faktor = 300
-iir_faktor = 0.8
-iir_order = 3
+iir_faktor = 0.8 # for vel_env 0.85
+iir_order = 3 # for vel_env 5
+
+
+# ADAPTIVE_PID_CONTROLLER
+amount_prev_observations = 20
 
 
 # AGENT
 input_dims = (amount_prev_observations * 3 + 5,)
 n_actions = 3
-
 project_root = os.path.dirname(__file__)
 chkpt_dir = os.path.join(project_root, 'adaptive_pid_controller', 'checkpoints', env)
 layer_sizes = [256, 256, 128, 64, 32]
 batch_size = 256
 action_space_high = 1
 
+
+# TRAINING
 load_checkpoint = False
 save_model = False
-learn = False
-
-
-# pos_env_iir_faktor = 0.8
-# pos_env_iir_order = 3
-# vel_env_iir_faktor = 0.85
-# vel_env_iir_order = 5
+learn = True
