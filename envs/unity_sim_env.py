5be7024b536ed272b02ca572bae7e89276f65585
Andreas Muehlmann <andi.muehlmann@arcor.de>
2022-07-02 22:33:57 +0200

    fix naming errors

--- envs/unity_sim_env.py
+++ envs/unity_sim_env.py
@@ -1,21 +1,14 @@
 from pos_env import PosEnv
 
 
-# TODO: make default values in PosEnv
-# TODO: everything with init (no init_values) call super init in the end for default values
-# TODO: should_reset into controller env
-# TODO: adjust values to UnitySim
-# TODO: make pid_controller faktor adjust to environment (inherit from pid_controller)
-# TODO: maybe remove self.faktor from self.env_force
-
-
 class UnitySimEnv(PosEnv):
     def __init__(self):
         super(UnitySimEnv, self).__init__()
 
     def _init_values(self):
-        self.inaccuracy = 0.05
+        self.inaccuracy = 0.2
 
+        # this is not up to date
         self.range_positive_reward = 0.05
         self.bad_error = 0.2
         self.max_positive_reward = self.bad_error * 10
@@ -25,18 +18,15 @@ class UnitySimEnv(PosEnv):
         self.time_without_big_target_change = 4
         self.time_without_env_force_change = 3
 
-        self.max_faktor = 1 / 4
+        self.max_faktor = 1
         self.min_faktor = self.max_faktor
 
-        self.max_env_force = -1
+        self.max_env_force = -0
         self.min_env_force = self.max_env_force
 
-        self.max_target =  0.3 # 1
+        self.max_target = 30
         self.min_target = -self.max_target
 
-        self.max_output = 1000
-        self.min_output = -self.max_output
-
-        self.max_small_target_change = 0.05
+        self.max_small_target_change = 3
         self.max_big_target_change = abs(self.max_target) + abs(self.min_target)
         self.max_env_force_change = abs(self.max_env_force) + abs(self.min_env_force)
--- init_controller.py
+++ init_controller.py
@@ -6,9 +6,9 @@ from adaptive_pid_controller import AdaptivePidController
 def init_controller():
     if conf.controller == 'pid_controller':
         return PidController(conf.p_faktor, conf.i_faktor, conf.d_faktor,
-                            conf.iir_faktor, conf.iir_order, conf.max_output_controller)
+                            conf.iir_faktor, conf.iir_order, conf.max_output)
     if conf.controller == 'adaptive_pid_controller':
         return AdaptivePidController(0, 0, 0, conf.iir_faktor, conf.iir_order,
-                                    conf.max_output_controller)
+                                    conf.max_output)
     else:
         raise Exception(f'no controller with name {conf.env} defined.')
