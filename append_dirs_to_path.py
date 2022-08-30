0ab772ab22426d30042c9f7daffd2e3a47282d1a
Andreas Muehlmann <andi.muehlmann@arcor.de>
2022-08-30 10:50:30 +0200

    different imu modul

--- append_dirs_to_path.py
+++ append_dirs_to_path.py
@@ -1,10 +1,8 @@
 import sys
 import os
 
-
 def append_dirs_to_path():
     current_dir = os.path.dirname(os.path.abspath(__file__))
-
     _append_child(current_dir, 'pid_controller')
     _append_child(current_dir, 'adaptive_pid_controller')
     _append_child(current_dir, 'envs')
--- interfaces/hardware_interface.py
+++ interfaces/hardware_interface.py
@@ -1,34 +1,57 @@
+import os
+import sys
+import time
+import smbus
+import numpy as np
+
+from imusensor.MPU9250 import MPU9250
+from imusensor.filters import kalman
+
 import RPi.GPIO as gpio # this just works on a raspberry pi
-from time import sleep
 
 from interface_control import InterfaceControl
-from mpu9250_jmdev.registers import *
-from mpu9250_jmdev.mpu_9250 import MPU9250
 
 
 class HardwareInterface(InterfaceControl):
     def __init__(self):
-        gpio.setmode(gpio.BOARD)
+        gpio.setmode(gpio.BCM)
         self.frequency_I2C = 50
-        self.base_duty = 5000
+        self.base_duty = 5
+        self.max_duty = 8
 
         self.pwm_pins = [_give_setup_pin(22), _give_setup_pin(23), _give_setup_pin(26), _give_setup_pin(20)]
 
-        self.mpu = self._give_set_up_mpu_sensor()
+        self.imu = self._give_set_up_imu()
+        self.calibrate_sensor()
 
-    def _give_set_up_mpu_sensor(self):
-        mpu = MPU9250(
-            address_ak=AK8963_ADDRESS,
-            address_mpu_master=MPU9050_ADDRESS_68,
-            address_mpu_slave=None,
-            bus=1,
-            gfs=GFS_1000,
-            afs=AFS_8G,
-            mfs=AK8963_BIT_16,
-            mode=AK8963_MODE_C100HZ)
+        self.sensorfusion = kalman.Kalman()
 
-        mpu.configure()
-        return mpu
+        self.imu.readSensor()
+        self.imu.computeOrientation()
+        self.sensorfusion.roll = self.imu.roll
+        self.sensorfusion.pitch = self.imu.pitch
+        self.sensorfusion.yaw = self.imu.yaw
+
+        self.current_time = time.time()
+
+    def _give_set_up_imu(self):
+        address = 0x68
+        bus = smbus.SMBus(1)
+        imu = MPU9250.MPU9250(bus, address)
+        imu.begin()
+        return imu
+
+    def read_calibration(self):
+        self.imu.loadCalibDataFromFile("/home/dronepi/calib_real_bolder.json")
+
+    def calibrate_sensor(self):
+        self.imu.caliberateAccelerometer()
+        print ("Acceleration calib successful")
+
+        self.imu.caliberateMagPrecise()
+        print ("Mag calib successful")
+
+        # TODO: write the calibration
 
     def _set_up_output(self):
         for pwm_pin in pwm_pins:
@@ -42,11 +65,18 @@ class HardwareInterface(InterfaceControl):
         return pwm
 
     def give_measurements(self):
-        print("Accelerometer", mpu.readAccelerometerMaster())
-        print("Gyroscope", mpu.readGyroscopeMaster())
-        print("Magnetometer", mpu.readMagnetometerMaster())
-        print("Temperature", mpu.readTemperatureMaster())
-        print("\n")
+        self.imu.readSensor()
+        self.imu.computeOrientation()
+        self.newTime = time.time()
+        self.dt = newTime - current_time
+        self.current_time = newTime
+
+        self.sensorfusion.computeAndUpdateRollPitchYaw(
+            imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2], imu.GyroVals[0], imu.GyroVals[1], imu.Gyro[2],
+            imu.MagVals[0], imu.MagVals[1], imu.MagVals[2], dt
+        )
+
+        return [self.sensorfusion.roll, self.sensorfusion.pitch, self.sensorfusion.yaw]
 
     def send_outputs(self, outputs):
         for pin_rotor, output in zip(self.pwm_pins, outputs):
