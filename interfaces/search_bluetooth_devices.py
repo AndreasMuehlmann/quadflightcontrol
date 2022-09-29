afa1a7bd29c8e38d38f619c5806aa05621660986
Andreas Muehlmann <andi.muehlmann@arcor.de>
2022-09-29 10:48:30 +0200

    changing sensor_calib

--- interfaces/search_bluetooth_devices.py
+++ interfaces/search_bluetooth_devices.py
@@ -1,5 +1,8 @@
 import bluetooth
 
+# make device discoverable with "sudo hciconfig hci0 piscan"
+
+
 nearby_devices = bluetooth.discover_devices(lookup_names=True)
 print("Found {} devices.".format(len(nearby_devices)))
 
