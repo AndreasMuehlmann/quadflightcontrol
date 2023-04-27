17dba335b4896e224ca809208cdffb890b73e5b5
Andreas Muehlmann <andi.muehlmann@arcor.de>
2023-04-27 09:58:51 +0200

    some fixes to test out on drone

--- interfaces/bno055_interface.py
+++ interfaces/bno055_interface.py
@@ -16,7 +16,6 @@ class BNO055_Interface():
         self.euler = [0, 0, 0]
 
         self.yaw = self.correct_yaw(self.base_euler[0])
-        self.rotation = 0
 
     def give_rotor_angles(self):
         new_euler = self.bno055.euler
@@ -29,23 +28,17 @@ class BNO055_Interface():
 
         return [-self.euler[1], self.euler[2], self.euler[1], -self.euler[2]]
 
-
-    def give_rotation(self):
+    def give_yaw(self):
         new_euler = self.bno055.euler
         if None in new_euler:
             print('None in euler')
-            return self.rotation
+            return self.yaw
         new_yaw = self.correct_yaw(new_euler[0])
-        if (new_yaw < 0 and self.yaw > 0) or (new_yaw > 0 and self.yaw < 0):
-            self.yaw = new_yaw
-            return self.rotation
-        new_rotation = (new_yaw - self.yaw) * 1 / conf.frequency
-        if self.is_differece_to_big([new_rotation], [self.rotation], 0.5):
+        if self.is_differece_to_big([self.yaw], [new_yaw], 10):
             print('give_rotation')
-            return self.rotation
+            return self.yaw
         self.yaw = new_yaw
-        self.rotation = new_rotation
-        return self.rotation
+        return self.yaw
 
     def correct_yaw(self, yaw):
         new_yaw = yaw - self.base_euler[0]
@@ -55,22 +48,23 @@ class BNO055_Interface():
     def is_differece_to_big(self, new_vector, old_vector, cut_of):
         count = 0
         for new_val, val in zip(new_vector, old_vector):
-            count  += 1
+            count += 1
             if abs(new_val - val) > cut_of:
                 print(f'unlogical value in measurement at index {count} in ', end='')
                 return True
         return False
-    
-    def give_height_vel(self, previous_height_vel):
+
+    def give_altitude(self, previous_altitude, altitude_vel):
         #print(f'{time.time()},{self.bno055.linear_acceleration[0]},{self.bno055.linear_acceleration[1]},{self.bno055.linear_acceleration[2]}')
         linear_acceleration = self.bno055.linear_acceleration
         gravity = self.bno055.gravity
         if None in gravity or None in linear_acceleration:
-            return previous_height_vel
+            print('None in gravity or linear_acceleration')
+            return self.altitude + self.altitude_vel * 1 / conf.frequency
         absolut_linear_height_acceleration = gravity[2] / sum(gravity) * sum(linear_acceleration)
         if self.is_differece_to_big([absolut_linear_height_acceleration], [0], 30):
-            print('give_height_vel')
-            return previous_height_vel
-        height_vel = previous_height_vel + absolut_linear_height_acceleration *  1 / conf.frequency
+            print('give_altitude')
+            return self.altitude + self.altitude_vel * 1 / conf.frequency
+        altitude = altitude_vel * conf.frequency + absolut_linear_height_acceleration *  conf.frequency**2 / 2
         # print(f'{[round(ala, 2) for ala in absolut_linear_height_acceleration] }')
-        return height_vel
+        return altitude
--- run_episode.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python live_plot.py &
-python run_episode.py 
