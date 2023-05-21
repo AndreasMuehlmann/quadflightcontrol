b1f45825eb26655c47b0400307834defc1b79f06
Andreas Muehlmann <andi.muehlmann@arcor.de>
2023-05-21 20:02:29 +0200

    refactored flight_control and encapsulate logging

--- flight_control.py
+++ flight_control.py
@@ -1,25 +1,22 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-import math
-import time
 
 import pygame
 
 import config as conf
 from bluetooth_raspberry_interface import BluetoothRaspberryInterface
-from data_sender import DataSender
 from hardware_interface import HardwareInterface
 from iir_filter import IirFilter
-from csv_writer import Csv_Writer
 from pid_flight_control import PidFlightControl
+from logging import Logging
 
 
 class FlightControl():
     def __init__(self):
         self.controller = PidFlightControl()
-
         self.interface_user = BluetoothRaspberryInterface()
         self.interface_control = HardwareInterface()
+        self.logging = Logging()
 
         self.clock = pygame.time.Clock()
         self.time = 0
@@ -28,16 +25,6 @@ class FlightControl():
         self.yaw_filter = IirFilter(0.6, 1)
         self.altitude_filter = IirFilter(0.8, 1)
         self.rotor_output_filters = [IirFilter(0.6, 1) for _ in range(4)]
-
-        measurements_field_names = ['time', 'froll', 'fpitch', 'fyaw', 'altitude*100', 'faltitude*100']
-        outputs_field_names = ['time', 'roll_c_o', 'pitch_c_o', 'yaw_c_o', 'altitude_c_o']
-        self.measurements_csv_writer = Csv_Writer('measurements.csv', measurements_field_names)
-        self.outputs_csv_writer = Csv_Writer('outputs.csv', outputs_field_names)
-        self.data_sender = DataSender()
-        for _ in range(50):
-            self.data_sender.send_message('field_names:' + ','.join(measurements_field_names) \
-                                          + ';' + ','.join(outputs_field_names))
-            time.sleep(0.01)
 
     def run(self):
         while True:
@@ -50,39 +37,39 @@ class FlightControl():
 
             rotor_angles = self.interface_control.give_rotor_angles()
             yaw = self.interface_control.give_yaw()
-            altitude = self.interface_control.give_altitude()
+            self.altitude = self.interface_control.give_altitude()
 
-            filtered_rotor_angles = self._give_filtered_list(rotor_angles,
+            self.filtered_rotor_angles = self._give_filtered_list(rotor_angles,
                                                              self.angle_filters)
-            filtered_altitude = self.altitude_filter.give_filtered(altitude)
-            filtered_yaw = self.yaw_filter.give_filtered(yaw)
+            self.filtered_altitude = self.altitude_filter.give_filtered(self.altitude)
+            self.filtered_yaw = self.yaw_filter.give_filtered(yaw)
 
-            rotor_outputs = self.controller.give_outputs(inputs, filtered_rotor_angles,
-                                                         filtered_yaw, filtered_altitude)
+            rotor_outputs = self.controller.give_outputs(inputs, self.filtered_rotor_angles,
+                                                         self.filtered_yaw, self.filtered_altitude)
             rotor_outputs = self._give_filtered_list(rotor_outputs, self.rotor_output_filters)
 
-            measurements = [self.time] + filtered_rotor_angles[:2] + \
-                [filtered_yaw, altitude * 100, filtered_altitude * 100]
-            outputs = [self.time] + self.controller.rotor_outputs_angle_controllers[:2] \
-                + [self.controller.yaw_controller_output, self.controller.altitude_controller_output]
-            measurements = [str(element) for element in measurements]
-            outputs = [str(element) for element in outputs]
-            self.measurements_csv_writer.add_line_of_data(measurements)
-            self.outputs_csv_writer.add_line_of_data(outputs)
-            self.data_sender.send_message(','.join(measurements) + ';' + ','.join(outputs))
-
             if not self.interface_user.should_flight_control_run():
-                self.reset() 
+                self.reset()
                 continue
 
-            shutdown_condition = abs(filtered_altitude) > 2 or abs(filtered_yaw) > 140 \
-                or abs(rotor_angles[0]) > 60 or abs(rotor_angles[1]) > 60
-            if shutdown_condition:
+            if self.should_turn_off():
                 self.turn_off()
                 break
 
             self.interface_control.send_outputs(rotor_outputs)
             self.time += 1 / conf.frequency
+
+    def _give_to_log_measurements(self):
+        return [self.time] + self.filtered_rotor_angles[:2] + \
+               [self.filtered_yaw, self.altitude * 100, self.filtered_altitude * 100]
+
+    def _give_to_log_outputs(self):
+        return [self.time] + self.controller.rotor_outputs_angle_controllers[:2] \
+            + [self.controller.yaw_controller_output, self.controller.altitude_controller_output]
+
+    def _should_turn_off(self):
+        return abs(self.filtered_altitude) > 2 or abs(self.filtered_yaw) > 140 \
+                or abs(self.filtered_rotor_angles[0]) > 20 or abs(self.filtered_rotor_angles[1]) > 20
 
     def _give_filtered_list(self, values, filters):
         filtered_values = []
